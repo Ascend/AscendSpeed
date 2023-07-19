db9c25bdd942db4e6f330dd5edc4bddb25bafda3
fengliangjun <f00812941@china.huawei.com>
2023-07-19 10:20:40 +0800

    llama modify

--- examples/llama/pretrain_llama_ptd_16B.sh
+++ examples/llama/pretrain_llama_ptd_16B.sh
@@ -1,19 +1,20 @@
-# This is an example: train llama using TD,
+# This is an example: train llama using PTD,
 # the number of parameters is not aligned
-export LD_LIBRARY_PATH=/usr/local/lib:/root/miniconda3/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/lib:/usr/local/lib:/root/miniconda3/lib:$LD_LIBRARY_PATH
 export HCCL_CONNECT_TIMEOUT=1200
-source /usr/local/Ascend/ascend-toolkit/set_env.sh
+export COMBINED_ENABLE=1
+
 GPUS_PER_NODE=8
 # Change for multinode config
 MASTER_ADDR=localhost
-MASTER_PORT=6000
+MASTER_PORT=6001
 NNODES=1
 NODE_RANK=0
 WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
 
 DATA_PATH=./dataset/llama_text_document
 CHECKPOINT_PATH=./ckpt
-
+rm -rf ./ckpt/*
 DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
 
 python -m torch.distributed.launch $DISTRIBUTED_ARGS \
@@ -21,11 +22,13 @@ python -m torch.distributed.launch $DISTRIBUTED_ARGS \
        --DDP-impl local \
        --use-contiguous-buffers-in-ddp \
        --tensor-model-parallel-size 4 \
-       --num-layers 8 \
-       --hidden-size 4096 \
-       --num-attention-heads 32 \
-       --micro-batch-size 2 \
-       --global-batch-size 16 \
+       --pipeline-model-parallel-size 2 \
+       --num-layers 30 \
+       --hidden-size 6656 \
+       --ffn-hidden-size 17920 \
+       --num-attention-heads 52 \
+       --micro-batch-size 1 \
+       --global-batch-size 32 \
        --seq-length 2048 \
        --max-position-embeddings 2048 \
        --train-iters 500000 \
@@ -44,9 +47,9 @@ python -m torch.distributed.launch $DISTRIBUTED_ARGS \
        --weight-decay 1e-2 \
        --clip-grad 1.0 \
        --lr-warmup-fraction .01 \
-       --checkpoint-activations \
-       --log-interval 10 \
+       --log-interval 1 \
        --save-interval 10000 \
        --eval-interval 1000 \
        --eval-iters 10 \
+       --initial-loss-scale 4096.0 \
        --fp16 | tee logs/train.log
