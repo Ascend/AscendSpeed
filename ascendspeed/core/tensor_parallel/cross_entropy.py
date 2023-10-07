8d67f6f1d37262bedba43df1cad2e41dc9b5eee4
g00841271 <guoxinjie1@huawei.com>
2023-10-07 17:18:28 +0800

    tensor parallel v1到v3升级，目前提交的代码不影响正常功能（增加mappings.py与random.py）

--- ascendspeed/core/tensor_parallel/cross_entropy.py
+++ ascendspeed/core/tensor_parallel/cross_entropy.py
@@ -91,7 +91,6 @@ class _VocabParallelCrossEntropy(torch.autograd.Function):
             = ((K - 1) * (1 - alpha) / (K - 1)) * y_gt + (alpha / (K - 1)) * \sum_{i != gt} y_i
             = (K * (1 - alpha) - 1) / (K - 1)) * y_gt  + (alpha / (K - 1)) * \sum_{i} y_i
             = (1 - (alpha * K) / (K - 1)) * y_gt + ( (alpha * K) / (K - 1) ) * \sum_{i} y_i / K
-            From: https://github.com/NVIDIA/NeMo/blob/main/nemo/collections/common/losses/smoothed_cross_entropy.py
             """
             assert 1.0 > label_smoothing > 0.0
             smoothing = label_smoothing * vocab_size / (vocab_size - 1)
