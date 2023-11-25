14759c27045116b9a2b03c92138bb31a55166392
yangyuan667 <yangyuan42@huawei.com>
2023-11-25 16:06:28 +0800

    add adapter for cann fa

--- ascendspeed/te/ops/csrc/pybind.cpp
+++ ascendspeed/te/ops/csrc/pybind.cpp
@@ -18,7 +18,6 @@
 #include "common.h"
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("npu_flash_attention", &npu_flash_attention, "flash attention forward");
-    m.def("npu_flash_attention_grad", &npu_flash_attention_grad, "flash attention backward");
-    m.def("npu_add", &add, "test transformer boost add op");
+    m.def("npu_fusion_attention", &npu_flash_attention, "fusion attention forward");
+    m.def("npu_fusion_attention_grad", &npu_flash_attention_grad, "fusion attention backward");
 }
--- ascendspeed/te/ops/flash_attention.py
+++ ascendspeed/te/ops/flash_attention.py
@@ -5,41 +5,45 @@ import ascendspeed_te_ops
 
 class AscendFlashAttention(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, query, key, value, head_num, input_layout, pse=None, padding_mask=None, atten_mask=None, 
-                scale=1.0, keep_prob=1.0, pre_tockens=2147483647, next_tockens=2147483647, inner_precise=1,
-                gen_mask_parallel=True, sync=False):
+    def forward(ctx, query, key, value, head_num, input_layout, pse=None, padding_mask=None, 
+                atten_mask=None, scale=1., keep_prob=1., pre_tokens=2147483647, next_tokens=2147483647, 
+                inner_precise=0, prefix=None, sparse_mode=0, gen_mask_parallel=True, sync=False):
         
+        outputs = ascendspeed_te_ops.npu_fusion_attention(query, 
+                key, value, head_num, input_layout, pse, padding_mask, 
+                atten_mask, scale, keep_prob, pre_tokens, next_tokens, 
+                inner_precise, prefix, sparse_mode, gen_mask_parallel, sync)
+        attention_in, softmax_max, softmax_sum, softmax_in, seed, offset, numels = outputs
+        ctx.save_for_backward(
+            query, key, value, pse, padding_mask, atten_mask, attention_in, softmax_max,
+            softmax_sum, softmax_in)
         ctx.scale = scale
         ctx.input_layout = input_layout
         ctx.head_num = head_num
-        ctx.pre_tokens = pre_tockens
-        ctx.next_tokens = next_tockens
+        ctx.pre_tokens = pre_tokens
+        ctx.next_tokens = next_tokens
         ctx.inner_precies = inner_precise
         ctx.gen_mask_parallel = gen_mask_parallel
         ctx.sync = sync
-
-        outputs = ascendspeed_te_ops.npu_flash_attention(
-            query, key, value, head_num, input_layout, pse, padding_mask, 
-            atten_mask, scale, keep_prob, pre_tockens, next_tockens, 
-            inner_precise, gen_mask_parallel, sync)
-
-        attention_score, softmax_max, softmax_sum, softmax_out, seed, offset, numels = outputs
-        ctx.saved_for_backward(
-            query, key, value, pse, padding_mask, atten_mask, attention_score, softmax_max,
-            softmax_sum, softmax_out, seed, offset, numels
-            )
+        ctx.seed = seed
+        ctx.offset = offset
+        ctx.numels = numels
+        ctx.prefix = prefix
+        ctx.sparse_mode = sparse_mode
 
         return outputs
 
     @staticmethod
     def backward(ctx, grad_outputs):
-        query, key, value, pse, padding_mask, atten_mask, attention_score, softmax_max,\
-            softmax_sum, softmax_out, seed, offset, numels = ctx.saved_tensors
-        results = ascendspeed_te_ops.npu_flasg_attention_grad(
+        query, key, value, pse, padding_mask, atten_mask, attention_in, softmax_max,\
+            softmax_sum, softmax_in = ctx.saved_tensors
+        results = ascendspeed_te_ops.npu_fusion_attention_grad(
             query, key, value, grad_outputs, ctx.head_num, ctx.input_layout, pse, padding_mask, atten_mask,
-            softmax_max, softmax_sum, softmax_out, attention_score, ctx.scale, ctx.keep_prob, ctx.pre_tokens,
-            ctx.next_tokens, ctx.inner_precise, seed, offset, numels, ctx.gen_mask_parallel, ctx.sync)
-        return results
+            softmax_max, softmax_sum, softmax_in, attention_in, ctx.scale, ctx.keep_prob, ctx.pre_tokens,
+            ctx.next_tokens, ctx.inner_precise, ctx.seed, ctx.offset, ctx.numels, ctx.prefix, ctx.sparse_mode,
+            ctx.gen_mask_parallel, ctx.sync)
+        
+        return results, None, None, None, None, None, None, None, None, None, None, None, None, None
 
 
 ascend_flash_attention = AscendFlashAttention.apply
